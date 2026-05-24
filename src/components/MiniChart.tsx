cfd29c11ca8334957c4084ebe2f5a8e515fb160f
itslavish <hxmmxd.khan@gmail.com>
2026-05-24 20:41:58 +0530

    Configure lightweight-charts options to explicitly disable watermark/branding overlays

--- src/components/MiniChart.tsx
+++ src/components/MiniChart.tsx
@@ -54,6 +54,9 @@ export default function MiniChart({ symbol, height = 120 }: MiniChartProps) {
       chart = createChart(containerRef.current, {
         width: containerRef.current.clientWidth,
         height: height,
+        watermark: {
+          visible: false,
+        },
         layout: {
           background: { type: ColorType.Solid, color: 'transparent' },
           textColor: '#6E6E73',
